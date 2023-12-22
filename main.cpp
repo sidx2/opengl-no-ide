79d05b8266fce3d655d46b3541a32e7b7085efe1
Siddhesh Pardeshi <siddheshpardeshi2@gmail.com>
2023-12-22 16:47:41 +0530

    Reading shaders from a shader file

--- main.cpp
+++ main.cpp
@@ -3,9 +3,46 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 #define uint unsigned int
 using namespace std;
+
+struct shaderProgramSource {
+    string VertexSource;
+    string FragmentSource;
+};
+
+static shaderProgramSource parseShader(const string &filepath) {
+    ifstream stream(filepath);
+    string line;
+    stringstream ss[2];
+
+    enum class shaderType {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+
+    shaderType type = shaderType :: NONE;
+
+    while(getline(stream, line)) {
+        if (line.find("#shader") != std::string::npos) {
+            if (line.find("vertex") != std::string::npos) {
+                // set mode to vertex
+                type = shaderType :: VERTEX;
+            }
+
+            else if (line.find("fragment") != std::string::npos) {
+                // set mode to fragment
+                type = shaderType :: FRAGMENT;
+            }
+        }
+
+        else ss[(int) type] << line << "\n";
+    }
+
+    return {ss[0].str(), ss[1].str()};
+}
 
 static unsigned int compileShader(unsigned int type, const string &source) {
     unsigned int id = glCreateShader(type);
@@ -86,27 +123,9 @@ int main(void)
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
     // shaders
-    const string vetexShader = 
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) in vec4 position;\n"
-        "\n"
-        "void main() {\n"
-        "   gl_Position = position;\n"
-        "}\n"
-    ;
+    shaderProgramSource src = parseShader("./res/shaders/basic.shader");
 
-    const string fragmentShader = 
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0) out vec4 color;\n"
-        "\n"
-        "void main() {\n"
-        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-        "}\n"
-    ;
-
-    uint shader = createShader(vetexShader, fragmentShader);
+    uint shader = createShader(src.VertexSource, src.FragmentSource);
     glUseProgram(shader);
 
     /* Loop until the user closes the window */
